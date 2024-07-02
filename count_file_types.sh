8d39435cd71a38414bfba9195e451d9618922d5c
johnny <johnny-hanna@outlook.com>
2024-07-02 20:08:49 +0300

    Fix count_file_types.sh script

--- count_file_types.sh
+++ count_file_types.sh
@@ -1,18 +1,26 @@
-if [ $1 ]; then
-    directory = $1
+#check if we have given the script a parameter
+if [ "$1" ]; then
+    directory=$1
 else
     echo "No Directory Entered"
     exit 1
 fi
 
-if [ls directory]; then
-    echo "Directory Found"
-else
+#check if it is a legit directory
+if [ ! -d "$directory" ]; then
     echo "No directory Found"
     exit 1
 fi
 
+#find the type for a given directroy, d:directory f:file l:link, and count with wc
+echo Directories : 
+find "$directory" -mindepth 1 -type d | wc -l
 
-directoryNb = $(find $directory -maxdepth 1 -type d)
+echo Regular files:
+find "$directory" -mindepth 1 -type f | wc -l
 
-echo $directoryNb
+echo Symbolic links:
+find "$directory" -mindepth 1 -type l | wc -l
+
+# wc -l: counts the number of lines
+# mindepth to not count the folder itself
